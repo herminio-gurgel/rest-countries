79e98fd54f5ca166c0f659553415bc7d4175eb19
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-07-22 12:42:43 -0300

    :wrench: chore: movendo as configurações do vuetify para um arquivo próprio

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,9 @@
-import './assets/main.scss'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import '@mdi/font/css/materialdesignicons.css'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import vuetify from './plugins/vuetify'
 
 import App from './App.vue'
 import router from './router'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  defaults: {
-    VMain: {
-      class: 'appWidth mx-auto',
-    },
-  },
-})
 
 const app = createApp(App)
 
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,18 @@
+import '../assets/main.scss'
+
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  defaults: {
+    VMain: {
+      class: 'appWidth mx-auto',
+    },
+  },
+})
+
+export default vuetify
