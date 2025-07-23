ad88febb1600cd0ca646b0846b50fa53d5eb687c
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-07-23 12:47:53 -0300

    🚚 feat: renomeando e montando os componentes em cada rota

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <h1>App page</h1>
-  <RouterView />
+  <v-app>
+    <v-main>
+      <h1>App page</h1>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import TheNavigationBar from '@/components/TheNavigationBar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/detail',
-      name: 'detail',
-      component: () => import('../views/DetailView.vue'),
+      component: TheNavigationBar,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: () => import('../views/HomeView.vue'),
+        },
+        {
+          path: 'detail',
+          name: 'detail',
+          component: () => import('../views/DetailView.vue'),
+        },
+      ],
     },
   ],
 })
