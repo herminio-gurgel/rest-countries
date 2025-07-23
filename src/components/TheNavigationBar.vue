3686b18ea320bf68508a1e07ca858fc5df93769a
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-07-23 12:46:06 -0300

    ✨ feat: adicionando o componente `TheNavigationBar`

---
+++ src/components/TheNavigationBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ThemeToggle from './ThemeToggle.vue'
+</script>
+
+<template>
+  <v-app-bar>
+    <v-row>
+      <v-app-bar-title>Where in the world?</v-app-bar-title>
+      <ThemeToggle />
+    </v-row>
+  </v-app-bar>
+  <RouterView />
+</template>
