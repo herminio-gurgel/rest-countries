6ee533871b599aed3b98e98334e7e7845166d951
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-07-23 12:30:56 -0300

    🔧 chore: movendo as configurações do pinia para um arquivo separado

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import vuetify from './plugins/vuetify'
+import pinia from './plugins/pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(vuetify)
 
