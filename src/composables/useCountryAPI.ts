59337e6a728a6ab0a6de93e0bc1fa9268a5e091e
Herminio Gurgel <herminio.gurgel@gmail.com>
2025-08-13 15:14:43 +0000

    ✨ feat: incluindo a função para pesquisar o país por código
    - Adicionando também a hidratação no campo `borders`

--- src/composables/useCountryAPI.ts
+++ src/composables/useCountryAPI.ts
@@ -1,10 +1,15 @@
-import { defineFields, getCountries } from '@yusifaliyevpro/countries'
+import {
+  defineFields,
+  getCountries,
+  getCountriesByCodes,
+  getCountryByCode,
+} from '@yusifaliyevpro/countries'
+import type { Cca3Code } from '@yusifaliyevpro/countries/types'
 import { ref } from 'vue'
 
 export function useCountryAPI() {
-  const countryFields = defineFields(['name', 'capital', 'population', 'region', 'flags', 'cca3'])
-
   const countries = ref()
+  const country = ref()
   const loading = ref(false)
   const error = ref(false)
 
@@ -13,7 +18,7 @@ export function useCountryAPI() {
     error.value = false
 
     countries.value = await getCountries({
-      fields: countryFields,
+      fields: defineFields(['name', 'capital', 'population', 'region', 'flags', 'cca3']),
     })
 
     if (!countries.value || countries.value.length === 0) {
@@ -25,10 +30,63 @@ export function useCountryAPI() {
     loading.value = false
   }
 
+  async function fetchCountryByCode(cca3: Cca3Code) {
+    const countryFields = defineFields([
+      'flags',
+      'name',
+      'population',
+      'region',
+      'subregion',
+      'capital',
+      'tld',
+      'currencies',
+      'languages',
+      'borders',
+    ])
+
+    loading.value = true
+    error.value = false
+
+    country.value = await getCountryByCode({
+      code: cca3,
+      fields: countryFields,
+    })
+
+    if (country.value === null) {
+      error.value = true
+      loading.value = false
+      return
+    }
+
+    if (country.value.borders && country.value.borders.length > 0) {
+      country.value.borders = await hydrateBorders(country.value.borders as Cca3Code[])
+    }
+
+    loading.value = false
+  }
+
+  async function hydrateBorders(borders: Cca3Code[]) {
+    if (!borders || borders.length === 0) return []
+
+    const borderCountries = await getCountriesByCodes({
+      codes: borders,
+      fields: defineFields(['name', 'cca3']),
+    })
+
+    if (!borderCountries || borderCountries.length === 0) return []
+
+    return borderCountries.map((borderCountry) => ({
+      code: borderCountry.cca3,
+      name: borderCountry.name.common,
+    }))
+  }
+
   return {
     countries,
+    country,
     loading,
     error,
     fetchCountries,
+    fetchCountryByCode,
   }
 }
